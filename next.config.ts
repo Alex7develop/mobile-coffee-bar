0b7b2bc074b2cec41e1dc46d9aff0343b3cbfa97
Alex7develop <aleksandr@MacBook-Pro-Aleksandr.local>
2026-01-18 21:46:56 +0300

    add_3d01

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    unoptimized: false,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
